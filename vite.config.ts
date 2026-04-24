aa6867a383379b13c2b6fb483ac5e505d69a74c4
Kaito.K <74711118+sc30gsw@users.noreply.github.com>
2026-04-25 06:55:52 +0900

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,63 @@
+import babel from "@rolldown/plugin-babel";
+import tailwindcss from "@tailwindcss/vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import { defineConfig } from "vite-plus";
+
+export default defineConfig({
+  fmt: {
+    ignorePatterns: ["**/routeTree.gen.ts"],
+    sortImports: {
+      partitionByComment: true,
+    },
+    sortPackageJson: {
+      sortScripts: true,
+    },
+    sortTailwindcss: {
+      functions: ["cn"],
+    },
+  },
+  lint: {
+    categories: {
+      correctness: "error",
+    },
+    env: {
+      browser: true,
+      node: true,
+    },
+    ignorePatterns: ["**/routeTree.gen.ts"],
+    options: {
+      denyWarnings: true,
+      typeAware: true,
+      typeCheck: true,
+    },
+    overrides: [
+      {
+        files: ["src/router.tsx", "*.config.ts"],
+        rules: {
+          "no-default-export": "off",
+        },
+      },
+    ],
+    plugins: ["react", "react-perf", "import", "jsx-a11y", "promise"],
+    rules: {
+      "no-default-export": "error",
+    },
+  },
+  staged: {
+    "*.{js,jsx,ts,tsx,json,css}": "vp check --fix",
+  },
+  plugins: [
+    tailwindcss(),
+    tanstackStart(),
+    // react's vite plugin must come after start's vite plugin
+    react(),
+    babel({ presets: [reactCompilerPreset()] }),
+  ],
+  resolve: {
+    tsconfigPaths: true,
+  },
+  test: {
+    include: ["src/**/*.test.ts", "src/**/*.test.tsx"],
+  },
+});
